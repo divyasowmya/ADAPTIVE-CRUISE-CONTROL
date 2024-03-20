0464ff74a13176e92b921e163cedf4c2ba27e87f
divyasowmya <158157685+divyasowmya@users.noreply.github.com>
2024-03-20 15:28:23 +0530

    #17622 AdaptiveCruiseControl.js
    Test activation and deactivation logic under various driving scenarios to ensure reliability and safety

---
+++ AdaptiveCruiseControl.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>Adaptive Cruise Control</h1>
+  <script>document.write("Test activation and deactivation logic under various driving scenarios to ensure reliability and safety 
+
+")</script>
+</body>
+</html>
